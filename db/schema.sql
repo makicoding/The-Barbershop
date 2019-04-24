ead4c9e4066d8a3f76854aea4e610b9786342aea
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-04-24 19:31:47 -0400

    schema added

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,15 @@ USE theBarbershop_db;
 
 CREATE TABLE appointments
 (
-	id INT NOT NULL AUTO_INCREMENT,
-    barber_id VARCHAR(100) NOT NULL,
-    date DATE NOT NULL,
-    time TIME NOT NULL,
-    available BOOLEAN NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
+	appointment_date DATETIME NOT NULL,
+	appointment_time DATETIME NOT NULL,
+	barber_last_name VARCHAR(100) NOT NULL,
+	barber_id INTEGER,
 	customer_first_name VARCHAR(100) NOT NULL,
-    customer_last_name VARCHAR(100) NOT NULL,
-    customer_email VARCHAR NOT NULL,
+	customer_last_name VARCHAR(100) NOT NULL,
+	customer_email VARCHAR(100) NOT NULL,
+	customer_phone VARCHAR(15) NOT NULL,
 	PRIMARY KEY (id)
 );
 
