2a6cc410d8e3262fbc570afcf45e29c0fef8f645
Maki Hirose <Capture@dyn-129-236-213-33.dyn.columbia.edu>
2019-04-29 19:12:58 -0400

    instruction comments added to bottom of server js and design elements mostly coded

--- server.js
+++ server.js
@@ -38,3 +38,87 @@ db.sequelize.sync({
     console.log("App listening on PORT " + PORT);
   });
 });
+
+
+
+// ----------------------------------------------------------------------------------------------------
+// INSTRUCTIONS AND ADDITIONAL NOTES
+
+/*
+.gitignore file
+
+For when uploading folder to a GitHub repository.
+
+Create .gitignore file at start of project and include inside it the following lines of code:
+node_modules
+.DS_Store
+
+It's good practice to always include node_modules in the .gitignore file because this folder can get huge.
+We have coded to "require" npm install of any packages necessary for this node app, so if someone clones this repo from
+GitHub, all they have to do is npm install any necessary packages for the app to work. Therefore the node_modules folder 
+doesn't need to be uploaded to GitHub.
+
+
+
+--------------------
+RUN SQL FILE TO CREATE DATABASE
+
+From the Mac Finder, open the schema.sql file in Sequel Pro.
+
+From the middle right of the page where there is a drop down menu that says "Run Previous",
+select "Run All Queries" from the drop down list. This will create the database.
+
+
+
+--------------------
+REFRESH DATABASE CONTENT
+
+To refresh the database content in Sequel Pro, press the "refresh table contents" button at the bottom
+of the "Content section" of the databse.  You could also press on your keyboard:
+// command r
+
+
+
+--------------------
+COMMAND LINE commands
+
+Before running server.js file in node, install package dependencies listed in package.json
+by typing either of the following into the command line:
+// npm i            // this is just a shorthand version of npm install
+// npm install
+
+
+
+To run node for server.js, type into the command line:
+// node server             // No need to type in .js, although node server.js will work too.
+
+
+
+If you need to exit at any point, type into the command line:
+// control c
+
+
+
+--------------------
+NODEMON
+
+Install (just once on your computer):
+npm install -g nodemon      // -g will install to the computer's root directory (system file) so it will be available globally outside the folder you are working on (i.e. from any folder on your computer)
+
+By using nodemon any changes you make to the server JS file will update on the server without having to restart the server every time.
+
+Run nodemon by typing into terminal:
+nodemon server.js (or nodemon server)
+
+
+
+--------------------
+IN THE BROWSER
+
+After running either node server or nodemon server, go to the Chrome Browser and type in:
+http://localhost:8080
+to connect to port 8080
+
+
+
+*/
